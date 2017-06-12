5ba82b83b339a36f9681e02b38683ddd75347fe8
VengefulNexus <VengefulNexusStudios@gmail.com>
2017-06-12 14:24:05 -0500

    Added User Control of bridge
    Press 'E' when near bridge control cube to toggle bridge (and change the navmesh accordingly)

--- Assets/bridgeControl.cs
+++ Assets/bridgeControl.cs
@@ -7,27 +7,33 @@ public class bridgeControl : MonoBehaviour {
     public Animator bridgeAnimator;
     public NavMeshObstacle AvoidCube;
 
-    public bool bridgeUp = false;
-    bool state = true;  //only true to immediately update animation and navMeshObstacle enabled
+    public bool bridgeStatus;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        setBridgeDown();
     }
 
     // Update is called once per frame
     void Update() {
-        if (bridgeUp != state)
-        {
-            if (bridgeUp)
-            {
-                AvoidCube.enabled = true;
-            } else
-            {
-                AvoidCube.enabled = false;
-            }
-            bridgeAnimator.SetBool("BridgeUp", bridgeUp);
-            state = bridgeUp;
-        }
-	}
 
+     }
+
+    public void setBridgeUp()
+    {
+        bridgeAnimator.SetBool("BridgeUp", true);
+        bridgeStatus = true;
+    }
+
+    public void setBridgeDown()
+    {
+        bridgeAnimator.SetBool("BridgeUp", false);
+        bridgeStatus = false;
+
+    }
+
+    public void updateAvoidCube()
+    {
+        AvoidCube.enabled = bridgeStatus;
+    }
 }
--- Assets/bridgeControlUI.cs
+++ Assets/bridgeControlUI.cs
@@ -7,6 +7,9 @@ public class bridgeControlUI : MonoBehaviour {
     public Transform playerTransform;
     public Transform buildingTransform;
     public GameObject UIPanel;
+    public bridgeControl Bridge;
+
+
     float xDistance;
     float yDistance;
 
@@ -14,43 +17,42 @@ public class bridgeControlUI : MonoBehaviour {
 
     float angle = 0.0f;
     bool playerInRange = false;
-
-    bool runOnAnim = false;
+    
 	// Use this for initialization
 	void Start () {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-	}
+        UIPanel.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
-
-
-
         if (Vector3.Distance(playerTransform.position, buildingTransform.position) < activeDistance)
         {
             playerInRange = true;
+            UIPanel.GetComponent<Animator>().Play("UIPanelAnimOn");
         } else
         {
             playerInRange = false;
+            UIPanel.GetComponent<Animator>().Play("UIPanelAnimOff");
         }
         if (playerInRange)
         {
-            if (!UIPanel.activeSelf)
-            {
-                UIPanel.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
-                UIPanel.SetActive(true);
-                runOnAnim = true;
-            }
             xDistance = buildingUIPanelPivot.position[0] - playerTransform.position[0];
             yDistance = buildingUIPanelPivot.position[2] - playerTransform.position[2];
             angle = -Mathf.Atan2(yDistance, xDistance) * Mathf.Rad2Deg;
             buildingUIPanelPivot.eulerAngles = new Vector3(0.0f, angle, 0.0f);
-        } else
-        {
-            if (UIPanel.activeSelf)
+
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                UIPanel.SetActive(false);
+                if (Bridge.bridgeStatus)
+                {
+                    Bridge.setBridgeDown();
+                    //Set button green or whatever
+                } else
+                {
+                    Bridge.setBridgeUp();
+                }
             }
         }
 	}
