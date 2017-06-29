190b996fa8425e8164a101187aafeb31b4df231a
Ghost114179 <eric114179@gmail.com>
2017-06-28 21:32:38 -0500

    Doppler Effect Testing

---
+++ Assets/TestDoppler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDoppler : MonoBehaviour {
+    public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += new Vector3(speed, 0.0f, 0.0f);
+	}
+}
